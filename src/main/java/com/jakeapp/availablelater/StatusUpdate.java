dcb1d0d2d924c8194f653bcbc068956539550d17
johannes <johannes@cfed719c-3465-4289-a5de-482d64f03f8e>
2009-03-22 19:14:09 +0000

    made availablelater a seperate project
    git-svn-id: svn://svn.jakeapp.com/trunk@2089 cfed719c-3465-4289-a5de-482d64f03f8e

---
+++ src/main/java/com/jakeapp/availablelater/StatusUpdate.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.jakeapp.availablelater;
+
+public class StatusUpdate {
+	public StatusUpdate(double progress, String status) {
+		super();
+		this.progress = progress;
+		this.status = status;
+	}
+	public double progress;
+	public String status;
+}
